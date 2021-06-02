3815a7449df540621430a2eeb9b0e012b85bb6b1
DanielRafik <73848754+DanielRafik@users.noreply.github.com>
2021-06-02 20:13:56 +0200

    Update station.h

--- ds-project-code/station/station.h
+++ ds-project-code/station/station.h
@@ -19,16 +19,39 @@ private:
 	Queue<mission> emergency_missions;
 	Queue<mission> polar_missions;
 
+	Queue<mission> EMwaiting_list;
+	Queue<mission> PMwaiting_list;
+
+	Queue<rover> ERwaiting_list;
+	Queue<rover> PRwaiting_list;
+
+	LinkedList<mission> EMInExecution_list;
+	LinkedList<mission> PMInExecution_list;
+
+	LinkedList<rover> ERInExecution_list;
+	LinkedList<rover> PRInExecution_list;
+
+	Queue<mission> DoneMissions_list;
 	bool resume;
 
-
+	int no_Provers, no_Erovers, V_Provers, V_Erovers;
+	int no_precheckMisions1, no_precheckMisions2, checkupDuration1, checkupDuration2;
+	int no_events;
+	char formulation, roverType;
+	int formulation_day, ID, targetLoc, missionDur, significance,eventDay;
 
 
 public:
 	station();
-	void get_input();
+	void get_input(int& no_events,  int& eventDay);
 	void print();
 	void execute();
 	bool get_resume();
+	Queue<mission> ended_E_missions(LinkedList<mission>& EMInExecution_list, LinkedList<rover>& ERInExecution);
+	Queue<mission> ended_P_missions(LinkedList<mission>& PMInExecution_list, LinkedList<rover>& PRInExecution);
+	/*Queue<rover> ended_E_rover(Queue<mission>& ERInExecution_list, Queue<rover>& ERInExecution);
+	Queue<rover> ended_P_rover(Queue<mission>& PRInExecution_list,Queue<rover>& PRInExecution);
+	*/
+	//void set_resume(bool r);
 };
 
