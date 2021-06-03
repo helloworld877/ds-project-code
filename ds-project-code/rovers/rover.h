8b4f3f8ecb43e7d19807e43accbd09c4ac62e571
DanielRafik <73848754+DanielRafik@users.noreply.github.com>
2021-06-04 00:56:12 +0200

    Update rover.h

--- ds-project-code/rovers/rover.h
+++ ds-project-code/rovers/rover.h
@@ -7,6 +7,7 @@ private:
 	bool available;
 	int missions_before_checkup;
 	int missions_done=0;
+	int days_incheckup=0;
 public:
 	rover(int speed, int missions_before_checkup, int checkup_duration);
 	rover();
@@ -19,11 +20,14 @@ public:
 	void set_availability(bool available);
 	
 	int get_missions_before_checkup();
+	
 
 	bool check_checkup();
 	void increment_missions();
 	int get_missions_done();
 
+	void set_days_incheckup(int &checkup);
+	int get_days_incheckup();
 
 	
 };
